50f6d2510fb14c0954b46cbe95db980a0576f157
Shen <bear_shen@live.cn>
2020-02-24 16:04:59 +0800

    lib fix

--- config.deploy.php
+++ config.deploy.php
@@ -5,19 +5,20 @@ global $extraConf;
 global $cache;
 //数据库配置项独立，通用的配置项直接取，其他的extra通过Settings取
 $dbConf    = [
-    'host' => '127.0.0.1',
-    'db'   => 'tiebaspider_v3',
-    'name' => 'root',
-    'pass' => 'root',
+    'host'    => '127.0.0.1',
+    'db'      => 'tiebaspider_v3',
+    'name'    => 'root',
+    'pass'    => 'root',
+    'charset' => 'utf8mb4',
 ];
-$conf  = [
-    'base' => [
+$conf      = [
+    'base'    => [
         'path' => __DIR__,
     ],
     'session' => [
-        'key' => 'ses_id',
+        'key'    => 'ses_id',
         'prefix' => 'frm:session:',
-        'expire' => 86400*180,
+        'expire' => 86400 * 180,
     ],
 ];
 $extraConf = [
@@ -42,4 +43,4 @@ $extraConf = [
         ],
     ]];
 //缓存
-$cache      = new \Predis\Client(/*'tcp://127.0.0.1:6379'*/);
+$cache = new \Predis\Client(/*'tcp://127.0.0.1:6379'*/);
--- lib/FuncCallable.php
+++ lib/FuncCallable.php
@@ -1,10 +1,26 @@
 <?php namespace Lib;
 
-
+/*
+ * @notice new self 不大确定是否应该写成默认，现在这种做法会导致在默认情况下每次都要新建实例
+ * 新建实例毫无疑问可以解决一些问题，但是性能上感觉并不划算
+ * */
 trait FuncCallable {
+    //通过设置 self:$saveSelf = true; ，则 _self 将会被储存为当前的实例，否则每次都会新建
+    //这项参数无法直接写在trait中，需要在被调用的类中手工添加
+    //private static $saveSelf = false;
+    private static $_self = false;
+
     public static function __callStatic($name, $arguments) {
         $name = '_' . $name;
-        return (new self)->$name(...$arguments);
+        $self = false;
+        if (isset(self::$saveSelf) && self::$saveSelf) {
+            if (!self::$_self) self::$_self = new self;
+            $self = self::$_self;
+//            var_dump('$load');
+        } else {
+            $self = new self;
+        }
+        return $self->$name(...$arguments);
     }
 
     public function __call($name, $arguments) {
