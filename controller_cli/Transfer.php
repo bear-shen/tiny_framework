d37b1d9b722c99c4ff6853b9ed3ffc71e0d5d054
Shen <bear_shen@live.cn>
2020-03-19 15:56:33 +0800

    transfer fixed

--- controller_cli/Transfer.php
+++ controller_cli/Transfer.php
@@ -13,6 +13,9 @@ use Lib\GenFunc;
 
 class Transfer extends Kernel {
 
+    /**
+     * loaded
+     */
     public function tranPostAct() {
         echo 'loading...' . "\r\n";
         $count = 0;
@@ -139,6 +142,9 @@ dbid,tid,pid,cid,is_lz,page_index,post_index,time_pub,time_scan,time_operate,use
         return $recordedUidList;
     }
 
+    /**
+     * loaded
+     */
     public function tranLoopAct() {
         echo 'loading...' . "\r\n";
 //        DB::query('insert into tiebaspider_v3.spd_looper
@@ -146,6 +152,7 @@ dbid,tid,pid,cid,is_lz,page_index,post_index,time_pub,time_scan,time_operate,use
 //SELECT pid,user_name,reason,group,status,time_add,time_operate
 // from tiebaspider_v2.looper');
         echo 'making target' . "\r\n";
+        DB::query('truncate tiebaspider_v3.spd_looper;');
         $resource = DB::query('select id,user_name,`group`,pid,reason,status,time_operate,time_add from tiebaspider_v2.looper');
         self::tick();
         $uidList = $this->getUidList(array_column($resource, 'user_name'));
@@ -172,9 +179,13 @@ dbid,tid,pid,cid,is_lz,page_index,post_index,time_pub,time_scan,time_operate,use
         return true;
     }
 
+    /**
+     * loaded
+     */
     public function tranKeywordAct() {
         self::line('truncating');
         DB::query('truncate tiebaspider_v3.spd_keyword');
+        DB::query('set autocommit = 0;');
         self::tick();
         self::line('writing');
         DB::query('insert into 
@@ -196,6 +207,16 @@ select
                 self::line('process:' . $i);
             }
             $userName = mb_trim($item['value']);
+            if (empty($userName)) {
+                self::line('empty after trim:' . $item['value']);
+                self::line($item);
+                continue;
+            }
+            if (empty($uidList[$userName])) {
+                self::line('lost uid:' . $userName);
+                self::line($item);
+                exit();
+            }
             DB::query(
                 'update spd_keyword set `value`=:value , position=2 where id=:id;',
                 [
@@ -209,14 +230,18 @@ select
 
     public function tranLogAct() {
         //创建临时表
-        $ts   = DB::query('SHOW TABLE STATUS from tiebaspider_v3;');
+        DB::query('truncate tiebaspider_v3.spd_operate_tmp;');
+        DB::query('truncate tiebaspider_v3.spd_operate;');
+        DB::query('truncate tiebaspider_v3.spd_operate_content;');
+        $ts = DB::query('SHOW TABLE STATUS from tiebaspider_v3;');
+//        DB::$logging = true;
         $incr = 0;
         foreach ($ts as $row) {
-            if ($row['Name'] != 'spd_log_operate') continue;
+            if ($row['Name'] != 'spd_operate') continue;
             $incr = $row['Auto_increment'];
             break;
         }
-        DB::query("create temporary table if not exists tiebaspider_v3.spd_log_operate_tmp(
+        DB::query("create table if not exists tiebaspider_v3.spd_operate_tmp(
 `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
 `post_id` bigint(20) unsigned NOT NULL , 
 `operate` tinyint(3) unsigned DEFAULT NULL,
@@ -226,7 +251,9 @@ select
 `operate_reason` text,
 `execute_result` text,
   PRIMARY KEY (`id`)
-) engine=Innodb auto_increment=${$incr}");
+) engine=Aria auto_increment=1;");
+//        var_dump(DB::getPdo()->errorInfo());
+        // auto_increment=${$incr}
         $count = 0;
         do {
             echo '--------------------------' . "\r\n";
@@ -248,22 +275,24 @@ execute_result
 -- content
  from tiebaspider_v2.log_operate 
  where operate != 1
- order by dbid desc
+ order by dbid asc
  limit 10000 offset {$offset} ;");
+//            var_dump(DB::getPdo()->errorInfo());
             if (empty($logs)) {
                 echo 'get empty log, end' . "\r\n";
                 self::tick(true);
-                return true;
+                break;
             }
             self::tick();
             echo 'round: ' . $count++ . ' loaded ' . sizeof($logs) . ' rows' . "\r\n";
-            $cidList    = array_column($logs, 'cid');
-            $posts      = DB::query("select 
+            $cidList = array_column($logs, 'cid');
+            $posts   = DB::query("select 
 id,cid
  from tiebaspider_v3.spd_post sp
 --  left join tiebaspider_v3.spd_post_content spc on sp.cid=spc.cid
 --  left join tiebaspider_v3.spd_user su on sp.uid=su.id
   where cid in (:v);", [], $cidList);
+//            var_dump(DB::getPdo()->errorInfo());
             $postsByCId = [];
             foreach ($posts as $item) {
                 $postsByCId[$item['cid']] = $item['id'];
@@ -298,20 +327,28 @@ id,cid
             }
             self::tick();
             echo 'insert tmp table' . "\r\n";
+            DB::query('insert into tiebaspider_v3.spd_operate_tmp 
+(post_id, operate, time_operate, time_execute, operate_id_list, operate_reason, execute_result) VALUES 
+(:v)', [], $insTmpArr);
+            echo 'insert tmp table finished' . "\r\n";
+//            break;
         } while (true);
         DB::query('insert ignore into 
 tiebaspider_v3.spd_operate 
 (id, post_id, operate, time_operate, time_execute) 
 select 
 id, post_id, operate, time_operate, time_execute 
-from spd_log_operate_tmp;');
+from spd_operate_tmp;');
+//        var_dump(DB::getPdo()->errorInfo());
         DB::query('insert ignore into 
 tiebaspider_v3.spd_operate_content 
 (id, operate_id_list, operate_reason, execute_result) 
 select 
 id, operate_id_list, operate_reason, execute_result
-from spd_log_operate_tmp
+from spd_operate_tmp
 ;');
+        var_dump(DB::getPdo()->errorInfo());
+//        var_dump(DB::$log);
         return true;
     }
 
