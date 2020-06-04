ea11f64612edefd2b2be63ea2158a269db872886
bear-shen <bear_shen@live.cn>
2020-06-04 15:51:29 +0800

    lib improve

--- lib/DB.php
+++ lib/DB.php
@@ -1,5 +1,7 @@
 <?php namespace Lib;
 
+use \PDO;
+
 /**
  * debug
  * 普通
@@ -26,35 +28,61 @@
  */
 class DB {
     use FuncCallable;
-    /** @var $dsn \PDO */
+
+    /** @var $dsn PDO */
     public static $pdo = null;
     /** @var $dsn string */
-    public static $dsn = '';
-    const BathKey = '(:k)';
-    const BathVal = '(:v)';
+    public static $dsn     = '';
     public static $logging = false;
     public static $log     = [];
     public static $split   = 1000;//@todo 好像还是不维护这玩意比较好。。。有空再做
+
+    const DirectReplacePrefix = ':';    //标识强制替换的参数头部
+    const BathKey             = '{:k}'; //批处理的key
+    const BathVal             = '{:v}'; //批处理的value
+    const BathBindPrefix      = 'BTH';//批处理参数的绑定头
 
     public function __construct() {
         global $dbConf;
         if (!self::$pdo) {
             self::$dsn = 'mysql:dbname=' . $dbConf['db'] . ';host=' . $dbConf['host'] . ';charset=' . $dbConf['charset'] . '';
 //            var_dump(self::$dsn);
-            self::$pdo = new \PDO(self::$dsn, $dbConf['name'], $dbConf['pass']);
+            self::$pdo = new PDO(self::$dsn, $dbConf['name'], $dbConf['pass']);
         }
     }
 
     /**
-     * @return \PDO
+     * @return PDO
      */
-    public static function getPdo(): \PDO {
-        if(self::$pdo)return self::$pdo;
+    public static function getPdo(): PDO {
+        if (self::$pdo) return self::$pdo;
         $self = new self;
         return self::$pdo;
     }
 
     /**
+     * @param string $query
+     *
+     * @param array $bind
+     * 常规绑定参数
+     * ['key_bind'=>'value',':key_replace'=>'value']
+     * 其中 :key_replace 可以强制替换 sql 文本中对应的绑定字段而不使用 pdo 的绑定方法
+     * // 目的是用来强制替换一些 sql 组件，有些不好绑定的数据就写这里面
+     * // 但是话说为什么不在 sql 上直接写完了传进来？
+     *
+     * @param array $args 第一个参数为普通的绑定数据，后面的都是批量数据，根据 sql 文本的顺序绑定
+     *
+     * 相对正常的 sql 写法有个限制，在批量写入时语法里不应该出现 ?
+     * 因为批量写入的函数会占用 ? 如果有的话批量的部分应该写在前面
+     * 实际上可以通过做计数来绕开，但是感觉没有必要
+     * 此外现在的写法多少不方便批量写入，最好想个办法优化一下
+     *
+     * available:
+     * [['key_bind'=>'value',],['key_bind'=>'value',],]
+     *
+     * @return array
+     *
+     * protected 为了方便调用
      * @see query
      *
      * select * from a where a = :a
@@ -63,41 +91,18 @@ class DB {
      * cast to insert into a ('','','') values ('','',''),('','','')
      * select * from spd_user where username in (:v) and pid in (:v)
      *
-     * @param string $query
-     * @param array $args 第一个参数为普通的绑定数据，后面的都是批量数据，根据sql文本的顺序绑定
-     *
-     * 相对正常的sql写法有个限制，在批量写入时语法里不应该出现 ? ，
-     * 因为批量写入的函数会占用 ? 如果有的话批量的部分应该写在前面
-     * 但是实际上可以通过做一次计数来绕开，但是感觉没有必要
-     * 此外现在的写法多少不方便批量写入，最好想个办法优化一下
-     *
-     * available:
-     * ['key_bind'=>'value',':key_replace'=>'value']
-     * [['key_bind'=>'value',],['key_bind'=>'value',],]
-     *
-     * @return array
-     *
-     * protected 为了方便调用
      */
     private function _query($query = '', $bind = [], ...$args) {
-        $datas = $args ?: [];
-        $bath  = $datas;
-        foreach ($bath as $k => $v) {
-            if (!is_numeric($k)) {
-                $bath = [$bath];
-                break;
-            }
-        }
-
-        //这个是强制替换sql组件的，有些不好绑定的数据就写这里面
+        $bath = $args ?: [];
         foreach ($bind as $key => $val) {
-            if (strpos($key, ':') !== 0) continue;
+            if (strpos($key, self::DirectReplacePrefix) !== 0) continue;
             $query = str_replace($key, $val, $query);
             unset($bind[$key]);
         }
 //        CliHelper::tick();
         //批量写入的部分
-        $toBind = [];
+        //@todo _generateBath修改成了bind形式，未测试
+        $bathBind = [];
         foreach ($bath as $bathItem) {
             $hitV = strpos($query, self::BathVal);
             if ($hitV !== false) {
@@ -108,7 +113,7 @@ class DB {
                         $query,
                         $bathK,
                         $hitK,
-                        strlen(self::BathKey)
+                        mb_strlen(self::BathKey, 'UTF-8')
                     );
                 }
                 //因为前面的replace，这里位置需要重新计算
@@ -120,7 +125,7 @@ class DB {
                     strlen(self::BathVal)
                 );
                 foreach ($bindV as $v) {
-                    $toBind[] = $v;
+                    $bathBind[] = $v;
                 }
             }
         }
@@ -128,33 +133,10 @@ class DB {
         $stat = self::$pdo->prepare($query);
         //绑定数据
         //批量插入的数据一定是?
-        foreach ($toBind as $k => $v) {
-            $key  = is_int($k) ? $k + 1 : $k;
-            $type = \PDO::PARAM_STR;
-            if (is_null($v)) {
-                $type = \PDO::PARAM_NULL;
-            } elseif (is_bool($v)) {
-                $type = \PDO::PARAM_BOOL;
-            }
-            $stat->bindValue($key, $v, $type);
-        }
+        $this->bathBind($stat, $bathBind);
+        $this->bathBind($stat, $bind);
 //        CliHelper::tick();
-        foreach ($bind as $k => $v) {
-            /**
-             * @see https://www.php.net/manual/zh/pdostatement.bindvalue.php
-             * ? 的索引从1开始
-             */
-            $key  = is_int($k) ? $k + 1 : $k;
-            $type = \PDO::PARAM_STR;
-            if (is_null($v)) {
-                $type = \PDO::PARAM_NULL;
-            } elseif (is_bool($v)) {
-                $type = \PDO::PARAM_BOOL;
-            }
-            $stat->bindValue($key, $v, $type);
-        }
-//        CliHelper::tick();
-        $stat->setFetchMode(\PDO::FETCH_NAMED);
+        $stat->setFetchMode(PDO::FETCH_NAMED);
 //        CliHelper::tick();
         if (self::$logging) {
             self::$log[] = [
@@ -173,28 +155,52 @@ class DB {
         return $stat->fetchAll();
     }
 
+    private function bathBind(&$statement, $data) {
+        /**
+         * @see https://www.php.net/manual/zh/pdostatement.bindvalue.php
+         * ? 的索引从1开始
+         */
+        foreach ($data as $key => $value) {
+            $key  = is_int($key) ? $key + 1 : $key;
+            $type = PDO::PARAM_STR;
+            if (is_null($value)) {
+                $type = PDO::PARAM_NULL;
+            } elseif (is_bool($value)) {
+                $type = PDO::PARAM_BOOL;
+            }
+            $statement->bindValue($key, $value, $type);
+        }
+    }
+
     /**
      * @see lastInsertId
      */
     private function _lastInsertId() {
         $stat = self::$pdo->prepare('select last_insert_id() as id;');
-        $stat->setFetchMode(\PDO::FETCH_NAMED);
+        $stat->setFetchMode(PDO::FETCH_NAMED);
         $stat->execute();
         $data = $stat->fetchAll();
         return $data[0]['id'];
     }
 
+    private int $bathCount = 0;
+
     /**
+     * 把数据转换成适合批处理的格式
+     *
      * @param array $data
+     * 输入强制两层，((a:1,b:1,),(a:2,b:2,),)
+     * 如果输入一层的话也会当成两层处理
+     *
      * @return array
      * [
-     *   '(`a`,`b`,`c`)',
-     *   '(?,?,?),(?,?,?),(?,?,?)',
-     *   '('a','a','a'),('b','b','b'),('c','c','c')',
+     *   '(`a`,`b`,`c`)',                             //string key，传入数值数组的时候同样会输出，根据 sql 的占用情况处理
+     *   '(':BTH0',':BTH1',':BTH2'),(':BTH3',':BTH4',':BTH5'),(':BTH6',':BTH7',':BTH8')',                   //string 绑定键
+     *   ['BTH0'=>'av','BTH1'=>'bv','BTH2'=>'cv',], //array 绑定值
      * ]
      */
     private function _generateBath($data = []) {
-        //data强制两层，((a:1,b:1,),(a:2,b:2,),)
+
         foreach ($data as $row) {
             if (is_array($row)) break;
             $data = [$data];
@@ -213,13 +219,19 @@ class DB {
             //data
             //merge很慢，弃用
             //$valArr = array_merge($valArr, array_values($row));
+            $valBindStr = [];
             foreach ($row as $col) {
-                $valArr[] = $col;
+                $bindKey         = self::BathBindPrefix . $this->bathCount;
+                $this->bathCount += 1;
+                //
+                $valArr[$bindKey] = $col;
+                $valBindStr[]     = ':'.$bindKey;
             }
+
 //            var_dump($valArr);
             //bind
-            $t            = array_fill(0, $colSize, '?');
-            $valBindArr[] = '(' . implode(',', $t) . ')';
+//            $t            = array_fill(0, $colSize, '?');
+            $valBindArr[] = '( ' . implode(' , ', $valBindStr) . ' )';
         }
 //        exit();
         $keyStr     = '(`' . implode('`,`', $keyArr) . '`)';
