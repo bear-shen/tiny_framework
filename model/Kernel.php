3100fbf3de9ca07e7d3b769ab594beaaf615fe93
shenjx <bear_shen@live.cn>
2021-02-09 03:14:19 +0800

    还是不用model吧...

--- model/Kernel.php
+++ model/Kernel.php
@@ -1,5 +1,12 @@
 <?php namespace Model;
 
-class Kernel {
+use Lib\ORM;
+
+/**
+ * model想想实在没什么用,估计也就到时候封装一些不想放控制器的方法进去...
+ * 像laravel的orm这种操作你得先搞定返回的collection
+ * 但是这样做就很麻烦,能不加还是先不加
+*/
+class Kernel extends ORM {
 
 }
