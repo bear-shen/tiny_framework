1fc233f3e2c4e6ee6f5d6bea1f44a6d8a73ae29a
shenjx <bear_shen@live.cn>
2021-03-03 23:07:31 +0800

    隔离出来

--- controller/File.php
+++ controller/File.php
@@ -24,7 +24,7 @@ class File extends Kernel {
         $sort   = Node::availSort($data['sort']);
         $status = Node::availStatus($data['type']);
         //
-        ORM::$logging=true;
+//        ORM::$logging=true;
         $indexList = ORM::table('node as nd')->
         leftJoin('node_index as ni', 'nd.id', 'ni.id')->
         where(function ($query) use ($data) {
@@ -45,202 +45,19 @@ class File extends Kernel {
             }
         })->where('nd.status', $status[0], $status[1])->
         page($data['page'])->order($sort[0], $sort[1])->
-        select(['ni.id', 'ni.list_tag',]);
+        select(['ni.id',]);
         $idList    = array_column($indexList, 'id');
         //$idArrList = array_flip($idList);
 //        var_dump($idList);
 //        var_dump(ORM::$log);
         if (empty($idList)) return $this->apiRet([]);
 
-        //-------------------------
-        $nodeList      = ORM::table('node as nd')->
-        leftJoin('node_info as ni', 'nd.id', 'ni.id')->
-        whereIn('nd.id', $idList)->
-        select(
+        return $this->apiRet(
             [
-                'nd.id',
-                'nd.id_parent',
-                'nd.status',
-                'nd.sort',
-                'nd.is_file',
-                'nd.time_create',
-                'nd.time_update',
-                'ni.name',
-                'ni.description',
-                'ni.id_cover',
-            ]
-        );
-        $fileIdList    = [];
-        $assocNodeList = [];
-        foreach ($nodeList as $node) {
-            $fileIdList[] = $node['id'];
-            if ($node['id_cover']) {
-                $fileIdList[] = $node['id_cover'];
-            }
-            $assocNodeList[$node['id']] = $node;
-        }
-
-        //-------------------------
-        $fileList      = ORM::table('file as fl')->
-        leftJoin('assoc_node_file as nf', 'fl.id', 'nf.id_file')->
-        whereIn('nf.id_node', $fileIdList)->
-        where('status', 1)->
-        select(
-            [
-                'fl.id',
-                'fl.hash',
-                'fl.suffix',
-                'fl.type',
-                'fl.size',
-                'nf.id_node',
-                'nf.status',
-            ]
-        );
-        $assocFileList = [];
-        foreach ($fileList as $file) {
-            $assocFileList[$file['id_node']] = $file;
-        }
-
-        //-------------------------
-        $tagIdList        = [];
-        $assocNodeTagList = [];
-        foreach ($indexList as $index) {
-            $nodeTagList = explode(',', $index['list_tag']);
-            $nodeTagList = array_filter($nodeTagList);
-            foreach ($nodeTagList as $tagId) {
-                $tagIdList[] = $tagId;
-            }
-            $assocNodeTagList[$index['id']] = $nodeTagList;
-        }
-        $assocTagInfoList = [];
-        if (!empty($tagIdList)) {
-            $tagIdList        = array_keys(array_flip($tagIdList));
-            $tagInfoList      = ORM::table('tag as tg')->leftJoin('tag_info as ti', 'tg.id', 'ti.id')->
-            where('tg.status', 1)->
-            whereIn('tg.id', $tagIdList)->select(
-                [
-                    'tg.id',
-                    'tg.id_group',
-                    //'tg.status',
-                    //'tg.time_create',
-                    //'tg.time_update',
-                    'ti.name',
-                    //'ti.alt',
-                    //'ti.description',
-                ]
-            );
-            $assocTagInfoList = GenFunc::value2key($tagInfoList, 'id');
-            //
-            $tagGroupList      = ORM::table('tag_group as tg')->leftJoin('tag_group_info as ti', 'tg.id', 'ti.id')->
-            where('tg.status', 1)->
-            whereIn('tg.id', $tagIdList)->select(
-                [
-                    'tg.id',
-                    'tg.id_node',
-                    //'tg.status',
-                    //'tg.time_create',
-                    //'tg.time_update',
-                    'ti.name',
-                    //'ti.alt',
-                    //'ti.description',
-                ]
-            );
-            $assocTagGroupList = GenFunc::value2key($tagGroupList, 'id');
-            foreach ($assocTagInfoList as $tagId => $tagInfo) {
-                $curGroup                 = ($assocTagGroupList[$tagInfo['id_group']] ?? []) + [
-                        'id'      => '',
-                        'id_node' => '',
-                        'name'    => '',
-                    ];
-                $assocTagInfoList[$tagId] += [
-                    'group_id'         => $curGroup['id'],
-                    'group_id_node'    => $curGroup['id_node'],
-                    'group_group_name' => $curGroup['group_name'],
-                ];
-            }
-        }
-
-        //-------------------------
-        $assocTargetList = [];
-        foreach ($indexList as $index) {
-            $curNode       = ($assocNodeList[$index['id']] ?? []) + [
-                    'id'          => '',
-                    'id_parent'   => '',
-                    'status'      => '',
-                    'sort'        => '',
-                    'is_file'     => '',
-                    'time_create' => '',
-                    'time_update' => '',
-                    'name'        => '',
-                    'description' => '',
-                    'id_cover'    => '',
-                ];
-            $curFile       = ($assocFileList[$index['id']] ?? []) + [
-                    'id'      => '',
-                    'hash'    => '',
-                    'suffix'  => '',
-                    'type'    => '',
-                    'size'    => '',
-                    'id_node' => '',
-                    'status'  => '',
-                ];
-            $curCover      = ($assocFileList[$curNode['id_cover']] ?? []) + [
-                    'id'      => '',
-                    'hash'    => '',
-                    'suffix'  => '',
-                    'type'    => '',
-                    'size'    => '',
-                    'id_node' => '',
-                    'status'  => '',
-                ];
-            $assocTargetList
-            [$index['id']] =
-                [
-                    'id'          => $curNode['id'],
-                    'raw'         => Node::getPath($curFile['hash'], $curFile['suffix'], $curFile['type'], 'raw', false),
-                    'normal'      => Node::getPath($curFile['hash'], $curFile['suffix'], $curFile['type'], 'normal', false),
-                    'cover'       => $curCover['id'] ? Node::getPath($curCover['hash'], $curCover['suffix'], $curCover['type'], 'preview', false) : '',
-                    'cover_id'    => $curNode['id_cover'],
-                    'title'       => $curNode['name'],
-                    'description' => $curNode['description'],
-                    'size'        => $curFile['size'],
-                    'hash'        => $curFile['hash'],
-                    'type'        => $curNode['is_file'] ? $curFile['type'] : 'folder',
-                    'favourite'   => $curNode['status'] == 2 ? 1 : 0,
-                    'time_create' => $curNode['time_create'],
-                    'time_update' => $curNode['time_update'],
-                    //tag id
-                    'tag'         => [],
-                ];
-            $nodeTags      = $assocNodeTagList[$index['id']] ?? [];
-//            var_dump($assocNodeTagList);
-            foreach ($nodeTags as $tagId) {
-                $tag = ($assocTagInfoList[$tagId] ?? []) + [
-                        'id'               => '',
-                        'id_node'          => '',
-                        'name'             => '',
-                        'group_id'         => '',
-                        'group_id_node'    => '',
-                        'group_group_name' => '',
-                    ];
-                if (empty($assocTargetList[$index['id']]['tag'][$tag['group_id']])) {
-                    $assocTargetList[$index['id']]['tag'][$tag['group_id']] = [
-                        'id'   => '',
-                        'name' => '',
-                        'sub'  => [],
-                    ];
-                }
-                $assocTargetList[$index['id']]['tag'][$tag['group_id']]['sub'][] = [
-                    'id'   => $tag['id'],
-                    'name' => $tag['name'],
-                ];
-            }
-            $assocTargetList[$index['id']]['tag'] = array_values(
-                $assocTargetList[$index['id']]['tag']
-            );
-            //explode(',', $index['list_tag'])
-        }
-        return $this->apiRet(array_values($assocTargetList));
+                'list' => [],
+                'navi' => [],
+                'dir'  => [],
+            ]);
     }
 
     public function modAct() {
--- model/Node.php
+++ model/Node.php
@@ -5,6 +5,203 @@ use Lib\GenFunc;
 use Lib\ORM;
 
 class Node {
+    public static function getNodeInfo($nodeIdList = [0], $withTag = true) {
+        if (!is_array($nodeIdList)) {
+            $nodeIdList = [$nodeIdList];
+        }
+        //-------------------------
+        $nodeList      = ORM::table('node as nd')->
+        leftJoin('node_info as ni', 'nd.id', 'ni.id')->
+        whereIn('nd.id', $nodeIdList)->
+        select(
+            [
+                'nd.id',
+                'nd.id_parent',
+                'nd.status',
+                'nd.sort',
+                'nd.is_file',
+                'nd.time_create',
+                'nd.time_update',
+                'ni.name',
+                'ni.description',
+                'ni.id_cover',
+            ]
+        );
+        $fileIdList    = [];
+        $assocNodeList = [];
+        foreach ($nodeList as $node) {
+            $fileIdList[] = $node['id'];
+            if ($node['id_cover']) {
+                $fileIdList[] = $node['id_cover'];
+            }
+            $assocNodeList[$node['id']] = $node;
+        }
+
+        //-------------------------
+        $fileList      = ORM::table('file as fl')->
+        leftJoin('assoc_node_file as nf', 'fl.id', 'nf.id_file')->
+        whereIn('nf.id_node', $fileIdList)->
+        where('status', 1)->
+        select(
+            [
+                'fl.id',
+                'fl.hash',
+                'fl.suffix',
+                'fl.type',
+                'fl.size',
+                'nf.id_node',
+                'nf.status',
+            ]
+        );
+        $assocFileList = [];
+        foreach ($fileList as $file) {
+            $assocFileList[$file['id_node']] = $file;
+        }
+
+        //-------------------------
+        $tagIdList        = [];
+        $assocNodeTagList = [];
+        $indexList        = ORM::table('node_index')->
+        whereIn('id', $nodeIdList)->select(['ni.id', 'ni.list_tag',]);
+        foreach ($indexList as $index) {
+            $nodeTagList = explode(',', $index['list_tag']);
+            $nodeTagList = array_filter($nodeTagList);
+            foreach ($nodeTagList as $tagId) {
+                $tagIdList[] = $tagId;
+            }
+            $assocNodeTagList[$index['id']] = $nodeTagList;
+        }
+        $assocTagInfoList = [];
+        if (!empty($tagIdList)) {
+            $tagIdList        = array_keys(array_flip($tagIdList));
+            $tagInfoList      = ORM::table('tag as tg')->leftJoin('tag_info as ti', 'tg.id', 'ti.id')->
+            where('tg.status', 1)->
+            whereIn('tg.id', $tagIdList)->select(
+                [
+                    'tg.id',
+                    'tg.id_group',
+                    //'tg.status',
+                    //'tg.time_create',
+                    //'tg.time_update',
+                    'ti.name',
+                    //'ti.alt',
+                    //'ti.description',
+                ]
+            );
+            $assocTagInfoList = GenFunc::value2key($tagInfoList, 'id');
+            //
+            $tagGroupList      = ORM::table('tag_group as tg')->leftJoin('tag_group_info as ti', 'tg.id', 'ti.id')->
+            where('tg.status', 1)->
+            whereIn('tg.id', $tagIdList)->select(
+                [
+                    'tg.id',
+                    'tg.id_node',
+                    //'tg.status',
+                    //'tg.time_create',
+                    //'tg.time_update',
+                    'ti.name',
+                    //'ti.alt',
+                    //'ti.description',
+                ]
+            );
+            $assocTagGroupList = GenFunc::value2key($tagGroupList, 'id');
+            foreach ($assocTagInfoList as $tagId => $tagInfo) {
+                $curGroup                 = ($assocTagGroupList[$tagInfo['id_group']] ?? []) + [
+                        'id'      => '',
+                        'id_node' => '',
+                        'name'    => '',
+                    ];
+                $assocTagInfoList[$tagId] += [
+                    'group_id'         => $curGroup['id'],
+                    'group_id_node'    => $curGroup['id_node'],
+                    'group_group_name' => $curGroup['group_name'],
+                ];
+            }
+        }
+
+        //-------------------------
+        $assocTargetList = [];
+        foreach ($indexList as $index) {
+            $curNode       = ($assocNodeList[$index['id']] ?? []) + [
+                    'id'          => '',
+                    'id_parent'   => '',
+                    'status'      => '',
+                    'sort'        => '',
+                    'is_file'     => '',
+                    'time_create' => '',
+                    'time_update' => '',
+                    'name'        => '',
+                    'description' => '',
+                    'id_cover'    => '',
+                ];
+            $curFile       = ($assocFileList[$index['id']] ?? []) + [
+                    'id'      => '',
+                    'hash'    => '',
+                    'suffix'  => '',
+                    'type'    => '',
+                    'size'    => '',
+                    'id_node' => '',
+                    'status'  => '',
+                ];
+            $curCover      = ($assocFileList[$curNode['id_cover']] ?? []) + [
+                    'id'      => '',
+                    'hash'    => '',
+                    'suffix'  => '',
+                    'type'    => '',
+                    'size'    => '',
+                    'id_node' => '',
+                    'status'  => '',
+                ];
+            $assocTargetList
+            [$index['id']] =
+                [
+                    'id'          => $curNode['id'],
+                    'raw'         => Node::getPath($curFile['hash'], $curFile['suffix'], $curFile['type'], 'raw', false),
+                    'normal'      => Node::getPath($curFile['hash'], $curFile['suffix'], $curFile['type'], 'normal', false),
+                    'cover'       => $curCover['id'] ? Node::getPath($curCover['hash'], $curCover['suffix'], $curCover['type'], 'preview', false) : '',
+                    'cover_id'    => $curNode['id_cover'],
+                    'title'       => $curNode['name'],
+                    'description' => $curNode['description'],
+                    'size'        => $curFile['size'],
+                    'hash'        => $curFile['hash'],
+                    'type'        => $curNode['is_file'] ? $curFile['type'] : 'folder',
+                    'favourite'   => $curNode['status'] == 2 ? 1 : 0,
+                    'time_create' => $curNode['time_create'],
+                    'time_update' => $curNode['time_update'],
+                    //tag id
+                    'tag'         => [],
+                ];
+            $nodeTags      = $assocNodeTagList[$index['id']] ?? [];
+//            var_dump($assocNodeTagList);
+            foreach ($nodeTags as $tagId) {
+                $tag = ($assocTagInfoList[$tagId] ?? []) + [
+                        'id'               => '',
+                        'id_node'          => '',
+                        'name'             => '',
+                        'group_id'         => '',
+                        'group_id_node'    => '',
+                        'group_group_name' => '',
+                    ];
+                if (empty($assocTargetList[$index['id']]['tag'][$tag['group_id']])) {
+                    $assocTargetList[$index['id']]['tag'][$tag['group_id']] = [
+                        'id'   => '',
+                        'name' => '',
+                        'sub'  => [],
+                    ];
+                }
+                $assocTargetList[$index['id']]['tag'][$tag['group_id']]['sub'][] = [
+                    'id'   => $tag['id'],
+                    'name' => $tag['name'],
+                ];
+            }
+            $assocTargetList[$index['id']]['tag'] = array_values(
+                $assocTargetList[$index['id']]['tag']
+            );
+            //explode(',', $index['list_tag'])
+        }
+        return array_values($assocTargetList);
+    }
+
     /**
      * 生成传入id的面包屑导航
      * @param array $nodeIdList [1,2,3] or 1
