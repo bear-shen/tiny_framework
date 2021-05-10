9c2cf3f7e96a4a323dd5c4aaa0eabf079351057c
bear_shen <bear_shen@live.cn>
2021-05-10 11:23:19 +0800

    f

--- job/Kernel.php
+++ job/Kernel.php
@@ -4,11 +4,13 @@ use Model\Settings;
 
 class Kernel {
     public function handle() {
-        $key = Settings::get('job.key');
+        $jobSettings = Settings::get('job');
+        $key         = $jobSettings['key'];
+        $interval    = $jobSettings['interval'];
         global $cache;
         while (true) {
             echo 'job running:' . microtime(true) . "\r\n";
-            usleep(1000000);
+            usleep($interval);
             $job = $cache->lpop($key);
             if (!$job) continue;
             list($jobClass, $jobData) = json_decode($job, true);
