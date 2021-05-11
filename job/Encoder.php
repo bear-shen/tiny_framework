bac9141329c42a30f8392d4027275c9c68d793f2
bear_shen <bear_shen@live.cn>
2021-05-12 07:33:39 +0800

    resizeimg

--- job/Encoder.php
+++ job/Encoder.php
@@ -6,18 +6,80 @@ use Lib\Request;
 use Model\File;
 
 class Encoder {
+    //编码处理类
     public function handle($data) {
-        $query = Request::query();
-        if (empty($query[1])) return 'no file id';
-        $fileData = File::where('id', $query[1])->selectOne();
+        $fileData = File::where('id', $data)->selectOne();
+        switch ($fileData['type']) {
+            case 'image':
+                $this->image($fileData);
+                break;
+            case 'video':
+                $this->video($fileData);
+                break;
+            case 'audio':
+                $this->audio($fileData);
+                break;
+        }
     }
 
-    public function image() {
+    public function image(File $file) {
+        $config = [
+            'normal'  => [
+                'max_width' => 2560,
+                'quality'   => 85,
+            ],
+            'preview' => [
+                'max_width' => 360,
+                'quality'   => 60,
+            ],
+        ];
+        //
+        $originPath                = File::getPathFromHash($file->hash, $file->suffix, $file->type, 'raw', true);
+        $config['normal']['path']  = File::getPathFromHash($file->hash, $file->suffix, $file->type, 'normal', true);
+        $config['preview']['path'] = File::getPathFromHash($file->hash, $file->suffix, $file->type, 'preview', true);
+        //
+        $img     = new \Imagick($originPath);
+        $originW = $img->getImageWidth();
+        $originH = $img->getImageHeight();
+        //preview
+        $previewImg = $img->clone();
+        $scaleRate  = max($originW, $originH) / $config['preview']['max_width'];
+        if ($scaleRate > 1) {
+            $previewImg->resizeImage($originH / $scaleRate, $originW / $scaleRate, \Imagick::FILTER_QUADRATIC, 1);
+            $previewImg->writeImage($config['preview']['path']);
+        } else {
+            copy($originPath, $config['preview']['path']);
+        }
+        //normal
+        $normalImg = $img->clone();
+        $scaleRate = max($originW, $originH) / $config['normal']['max_width'];
+        if ($scaleRate > 1) {
+            $normalImg->resizeImage($originH / $scaleRate, $originW / $scaleRate, \Imagick::FILTER_QUADRATIC, 1);
+            $normalImg->writeImage($config['normal']['path']);
+        } else {
+            copy($originPath, $config['normal']['path']);
+        }
+        //
+        return true;
     }
 
-    public function video() {
+    public function video(File $file) {
+        $config = [
+            'normal'  => [
+                'max_width' => 1280,
+            ],
+            'preview' => [
+                'max_width' => 360,
+                'quality'   => 60,
+            ],
+        ];
     }
 
-    public function audio() {
+    public function audio(File $file) {
+        $config = [
+            'normal' => [
+                'bit_rate' => 256,
+            ],
+        ];
     }
 }
